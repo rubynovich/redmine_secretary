d83e47c9a950649ec31023e290f1690c6e720bbd
Rubynovich <roman@shipiev.me>
2012-04-16 16:27:15 +0400

    add out#show

--- app/helpers/outgoing_letters_helper.rb
+++ app/helpers/outgoing_letters_helper.rb
@@ -1,2 +1,5 @@
 module OutgoingLettersHelper
+  def outgoing_letter_heading(object)  
+    [t(:field_outgoing_code), object.outgoing_code, t(:dated), format_date(object.created_on)].join(' ')
+  end
 end
