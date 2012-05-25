c713c7913b1e06103e110a3255ab55d03e9159e6
Rubynovich <roman@shipiev.me>
2012-05-25 14:32:07 +0400

    fix bug (position were null)

--- db/migrate/20120525081126_add_position_to_organization.rb
+++ db/migrate/20120525081126_add_position_to_organization.rb
@@ -1,6 +1,10 @@
 class AddPositionToOrganization < ActiveRecord::Migration
   def self.up
     add_column :organizations, :position, :integer, :default => 1
+    pos = 1
+    Organization.find_each do |org|
+      org.update_attribute(:position, pos+=1)
+    end    
   end
 
   def self.down
