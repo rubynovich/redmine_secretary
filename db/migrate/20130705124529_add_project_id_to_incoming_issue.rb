4160d5acd323c51310e43823237ad56a78f21253
rubynovich <roman@shipiev.me>
2013-07-05 17:04:23 +0400

    ops

--- db/migrate/20130705124529_add_project_id_to_incoming_issue.rb
+++ db/migrate/20130705124529_add_project_id_to_incoming_issue.rb
@@ -2,7 +2,7 @@ class AddProjectIdToIncomingIssue < ActiveRecord::Migration
   def change
     #add_column :incoming_issues, :project_id, :integer
     IncomingIssue.where(:project_id => nil).find_each do |incoming_issue|
-      incoming_issue.update_attribute(:project_id, incoming_issue.issue.project_id)
+      incoming_issue.update_attribute(:project_id, incoming_issue.issue.try(:project_id))
     end
   end
 end
