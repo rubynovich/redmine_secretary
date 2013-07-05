88f9e12b720458f8c8f135904bf8350364e59881
rubynovich <roman@shipiev.me>
2013-07-05 17:00:18 +0400

    ops

--- app/models/incoming_issue.rb
+++ app/models/incoming_issue.rb
@@ -8,7 +8,7 @@ class IncomingIssue < ActiveRecord::Base
   validates_presence_of :issue_id, :incoming_letter_id
   validates_uniqueness_of :issue_id, :scope => :incoming_letter_id
 
-  after_save :put_project_id
+  before_save :put_project_id
 
   def put_project_id
     self.update_attribute(:project_id, issue.project_id)
